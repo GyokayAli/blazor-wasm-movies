a32c72d99428f9eb6affad9ae3a2c4fa62a5d842
GyokayAli <gyokay.metin.ali@gmail.com>
2020-03-19 20:34:10 +0100

    Added first components and trying out render fragments

---
+++ BlazorMovies/Client/Helpers/StringUtilities.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorMovies.Client.Helpers
+{
+    public class StringUtilities
+    {
+        public static string CustomToUpper(string value) => value.ToUpper();
+    }
+}
---
+++ BlazorMovies/Shared/Entities/Movie.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BlazorMovies.Shared.Entities
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
