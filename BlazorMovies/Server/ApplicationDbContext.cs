51cd5d7c6901eda167326b6821d39c6d182e5876
GyokayAli <gyokay.metin.ali@gmail.com>
2020-04-06 15:33:19 +0100

    Started with visualize person component

--- BlazorMovies/Server/ApplicationDbContext.cs
+++ BlazorMovies/Server/ApplicationDbContext.cs
@@ -47,30 +47,30 @@ namespace BlazorMovies.Server
             //        UserId = userAdminId
             //    });
 
-            // Add Genres
-            modelBuilder.Entity<Genre>()
-                .HasData(
-                    new Genre { Id = 1, Name = "Comedy" },
-                    new Genre { Id = 2, Name = "Action" },
-                    new Genre { Id = 3, Name = "Horror" },
-                    new Genre { Id = 4, Name = "Romance" },
-                    new Genre { Id = 5, Name = "Thriller" },
-                    new Genre { Id = 6, Name = "Drama" },
-                    new Genre { Id = 7, Name = "Mystery" },
-                    new Genre { Id = 8, Name = "Crime" },
-                    new Genre { Id = 9, Name = "Animation" },
-                    new Genre { Id = 10, Name = "Fantasy" },
-                    new Genre { Id = 11, Name = "Comedy-Romance" },
-                    new Genre { Id = 12, Name = "Action-Comedy" },
-                    new Genre { Id = 13, Name = "Superhero" },
-                    new Genre { Id = 14, Name = "Sci-Fi" },
-                    new Genre { Id = 15, Name = "Biography" },
-                    new Genre { Id = 16, Name = "Documentary" },
-                    new Genre { Id = 17, Name = "Musical" },
-                    new Genre { Id = 18, Name = "Family" },
-                    new Genre { Id = 19, Name = "War" },
-                    new Genre { Id = 20, Name = "Western" }
-                );
+            //// Add Genres
+            //modelBuilder.Entity<Genre>()
+            //    .HasData(
+            //        new Genre { Id = 1, Name = "Comedy" },
+            //        new Genre { Id = 2, Name = "Action" },
+            //        new Genre { Id = 3, Name = "Horror" },
+            //        new Genre { Id = 4, Name = "Romance" },
+            //        new Genre { Id = 5, Name = "Thriller" },
+            //        new Genre { Id = 6, Name = "Drama" },
+            //        new Genre { Id = 7, Name = "Mystery" },
+            //        new Genre { Id = 8, Name = "Crime" },
+            //        new Genre { Id = 9, Name = "Animation" },
+            //        new Genre { Id = 10, Name = "Fantasy" },
+            //        new Genre { Id = 11, Name = "Comedy-Romance" },
+            //        new Genre { Id = 12, Name = "Action-Comedy" },
+            //        new Genre { Id = 13, Name = "Superhero" },
+            //        new Genre { Id = 14, Name = "Sci-Fi" },
+            //        new Genre { Id = 15, Name = "Biography" },
+            //        new Genre { Id = 16, Name = "Documentary" },
+            //        new Genre { Id = 17, Name = "Musical" },
+            //        new Genre { Id = 18, Name = "Family" },
+            //        new Genre { Id = 19, Name = "War" },
+            //        new Genre { Id = 20, Name = "Western" }
+            //    );
 
             base.OnModelCreating(modelBuilder);
         }
